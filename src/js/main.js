c8f70a769d207ab8cbdc85a795bf053c5ec2efb8
Dennys Morillo <dennyoscar13@gmail.com>
2022-05-25 05:37:52 -0400

    Testing

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+let fName = prompt("Eres Gaby?", "")
+
+if(fName === "Si" || fName === "si"){
+    const saludar = () => {
+        return alert('Eres lo más lindo que he tenido en este 2022')
+    } 
+    
+    saludar()
+
+}
+
+
